586bbda34d6fb25ea2d350d52bcc5ef290f4adad
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-08 19:49:45 +0530

    [NISHANT] add : Tic Tac Toe Board Shown

--- src/main/java/com/capg/workshop/TicTacToeGame/TicTacToeGame.java
+++ src/main/java/com/capg/workshop/TicTacToeGame/TicTacToeGame.java
@@ -3,32 +3,45 @@ package com.capg.workshop.TicTacToeGame;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+
 	static Scanner sc = new Scanner(System.in);
-	private static final char CHAR_X='X';
-	private static final char CHAR_O='O';
+
+	private static final char CHAR_X = 'X';
+	private static final char CHAR_O = 'O';
+
+	public static void main(String[] args) {
+
+		createBoard();
+		char playerChoice = sc.next().charAt(0);
+		computerChoice(playerChoice);
+		printBoard();
+	}
+
+	static char[] ticTacBoard;
 
 	public static char[] createBoard() {
-		char[] ticTacToeBoard = new char[10];
+		ticTacBoard = new char[10];
 		for (int i = 0; i < 10; i++) {
-			ticTacToeBoard[i] = ' ';
+			ticTacBoard[i] = ' ';
 		}
-		return ticTacToeBoard;
+		return ticTacBoard;
 	}
-	public static char playerChoice(char playerChoice) {
+
+	public static char computerChoice(char playerChoice) {
 		char computerChoice;
-		if(playerChoice == CHAR_X ) {
-			System.out.println("User Choice : X && Computer : O");
+		if (playerChoice == CHAR_X) {
 			computerChoice = CHAR_O;
-		}else {
-			System.out.println("User Choice : X && Computer : O");
-			computerChoice = CHAR_X;	
+		} else {
+			computerChoice = CHAR_O;
 		}
 		return computerChoice;
 	}
 
-	public static void main(String[] args) {
-		createBoard();
-		System.out.println("Enter your choice 'X' or 'O'"); 
-		playerChoice(sc.next().charAt(0));
+	public static void printBoard() {
+		System.out.println(" " + ticTacBoard[1] + " | " + ticTacBoard[2] + " | " + ticTacBoard[3]);
+		System.out.println("-----------");
+		System.out.println(" " + ticTacBoard[4] + " | " + ticTacBoard[5] + " | " + ticTacBoard[6]);
+		System.out.println("-----------");
+		System.out.println(" " + ticTacBoard[7] + " | " + ticTacBoard[8] + " | " + ticTacBoard[9]);
 	}
 }
